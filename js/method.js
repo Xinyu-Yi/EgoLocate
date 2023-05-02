c72b0ec080fc141289ba25d19ee6092f4ce056a9
yxy <yixy20@mails.tsinghua.edu.cn>
2023-05-02 22:42:47 +0800

    init

---
+++ js/method.js
@@ -0,0 +1,33 @@
+var img = document.getElementById("method-img");
+
+var s1 = document.getElementById("s1");
+s1.onmouseover = function() { img.src = "images/method-s1.png"; }
+s1.onmouseout = function() { img.src = "images/method.png"; }
+
+var s2 = document.getElementById("s2");
+s2.onmouseover = function() { img.src = "images/method-s2.png"; }
+s2.onmouseout = function() { img.src = "images/method.png"; }
+
+var s3 = document.getElementById("s3");
+s3.onmouseover = function() { img.src = "images/method-s3.png"; }
+s3.onmouseout = function() { img.src = "images/method.png"; }
+
+var s4 = document.getElementById("s4");
+s4.onmouseover = function() { img.src = "images/method-s4.png"; }
+s4.onmouseout = function() { img.src = "images/method.png"; }
+
+var in1 = document.getElementById("in1");
+in1.onmouseover = function() { img.src = "images/method-in1.png"; }
+in1.onmouseout = function() { img.src = "images/method.png"; }
+
+var in2 = document.getElementById("in2");
+in2.onmouseover = function() { img.src = "images/method-in2.png"; }
+in2.onmouseout = function() { img.src = "images/method.png"; }
+
+var out1 = document.getElementById("out1");
+out1.onmouseover = function() { img.src = "images/method-out1.png"; }
+out1.onmouseout = function() { img.src = "images/method.png"; }
+
+var out2 = document.getElementById("out2");
+out2.onmouseover = function() { img.src = "images/method-out2.png"; }
+out2.onmouseout = function() { img.src = "images/method.png"; }
